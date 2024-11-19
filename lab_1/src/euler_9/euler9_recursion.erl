f2a31c2c15dcc230cf8f4788d19448cebdb1d4b9
Denis Golikov <108273292+Denoske@users.noreply.github.com>
2024-11-19 11:27:40 +0300

    Update euler9_recursion.erl

--- lab_1/src/euler_9/euler9_recursion.erl
+++ lab_1/src/euler_9/euler9_recursion.erl
@@ -1,26 +1,43 @@
 -module(euler9_recursion).
--export([find_triplet/0]).
+-export([find_triplet/0, find_triplet_process/3]).
 
 find_triplet() ->
-    find_triplet(1, 2, 997).
+    % Запускаем процесс для поиска тройки
+    Pid = spawn(fun() -> find_triplet_process(1, 2, 997) end),
+    % Ждем ответа от процесса
+    receive
+        {found, Product} -> Product;
+        {error, Reason} -> {error, Reason}
+    after 5000 -> % Тайм-аут на 5 секунд
+        {error, timeout}
+    end.
 
-find_triplet(A, B, C) ->
+find_triplet_process(A, B, C) ->
+    % Проверяем условия
     case A + B + C of
         1000 ->
-            case A * A + B * B =:= C * C of
-                true -> A * B * C;
-                false -> find_next(A, B, C)
+            if A * A + B * B =:= C * C ->
+                % Отправляем найденный результат обратно
+                self() ! {found, A * B * C};
+            true ->
+                find_next(A, B, C)
             end;
         _ ->
             find_next(A, B, C)
     end.
 
 find_next(A, B, C) ->
+    % Переход к следующему состоянию
     case {B < C - 1, A < 999} of
         {true, _} ->
-            find_triplet(A, B + 1, C - 1);
+            % Имитация "лени" с задержкой
+            timer:sleep(random:uniform(100)),
+            find_triplet_process(A, B + 1, C - 1);
         {false, true} ->
-            find_triplet(A + 1, A + 2, 1000 - (A + 1 + (A + 2)));
+            % Имитация "лени" с задержкой
+            timer:sleep(random:uniform(100)),
+            find_triplet_process(A + 1, A + 2, 1000 - (A + 1 + (A + 2)));
         {false, false} ->
-            error
+            self() ! {error, no_triplet} % Отправляем сообщение об ошибке
     end.
+
